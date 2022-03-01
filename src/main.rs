bd4b1afe76a1571c5ce48bbee701fde4d004186c
sky5hr0ud <skyshroudabc@gmail.com>
2022-03-01 15:56:41 -0800

    added max, min, mean

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
     println!("Hello, world!");
+    let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
+    let max = max(&numbers);
+    let min = min(&numbers);
+    let mean = mean(&numbers);
+    assert_eq!(max, 56);
+    assert_eq!(min, -18);
+    assert_eq!(mean, 12.5);
+    println!("Tests Passed!");
+}
+
+fn max(numbers: &[i32]) -> i32 {
+    let mut max_num = numbers[0];
+    for number in numbers {
+        if number > &max_num {
+            max_num = *number
+        }
+    };
+    return max_num
+}
+
+fn min(numbers: &[i32]) -> i32 {
+    let mut min_num = numbers[0];
+    for number in numbers {
+        if number < &min_num{
+            min_num = *number
+        }
+    };
+    return min_num
+}
+
+fn mean(numbers: &[i32]) -> f64 {
+    let mut mean: f64 = 0.0;
+    for number in numbers {
+        mean += *number as f64
+    }
+    mean /= numbers.len() as f64;
+    return mean
 }
